dd7f0e5e8f38348b97b0239e3c8c626515e8a9e8
martinhejo <marhjoh@stud.ntnu.no>
2023-10-27 18:17:34 +0200

    Fix: modified the defined structs in structs.go

--- internal/webserver/structs/structs.go
+++ internal/webserver/structs/structs.go
@@ -1,42 +1,14 @@
 package structs
 
-import "time"
-
-// ReceiptInformation A struct to encode information.
-type ReceiptInformation struct {
-	ReceiptTitle string    `json:"receipt-title"`
-	FoodList     []string  `json:"food-list"`
-	Cache        time.Time // Time in cache.
+// FoodList A struct to encode information.
+type FoodList struct {
+	UserID   string      `json:"receipt-title"`
+	FoodList []FoodItems `json:"food-list"`
 }
 
-// FoodInformation A struct to encode information
-type FoodInformation struct {
-	Name   string `json:"name"`
-	Amount int    `json:"amount"`
-}
-
-type Webhook struct {
-	Url     string `json:"url"`
-	Country string `json:"country"`
-	Calls   int    `json:"calls"`
-	Event   string `json:"event"`
-}
-
-type WebhookID struct {
-	ID string `json:"webhook_id"`
-	Webhook
-	Created     time.Time `json:"created_timestamp"`
-	Invocations int       `json:"invocations"`
-}
-
-// The call response for any given webhook
-type WebhookCallResponse struct {
-	ID string `json:"webhook_id"`
-	Webhook
-	Invocations int    `json:"invocations"`
-	Message     string `json:"message"`
-}
-
-type IdResponse struct {
-	ID string `json:"webhook_id"`
+// FoodItems A struct to encode information
+type FoodItems struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Quantity int    `json:"amount"`
 }
