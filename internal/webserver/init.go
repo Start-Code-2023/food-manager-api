85b765398e6d10457062daa0e82a442889b1c265
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-28 16:35:20 +0200

    add: CORS handling for allowing locally running next app to use the api

--- internal/webserver/init.go
+++ internal/webserver/init.go
@@ -5,6 +5,8 @@ import (
 	"food-manager/internal/webserver/handlers"
 	"log"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 // InitServer sets up handler endpoints and starts the HTTP-server
@@ -17,7 +19,16 @@ func InitServer() {
 	http.HandleFunc(constants.CREATE_PATH, handlers.CreateFoodListDoc)
 	http.HandleFunc(constants.DELETE_PATH, handlers.DeleteFoodListDocHandler)
 
+	// Create the CORS handler with allowed headers, origins, and methods
+	corsHandler := cors.New(cors.Options{
+		AllowedHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "HEAD", "POST", "PUT", "OPTIONS"},
+	})
+
+	// Apply CORS middleware to the server
+	corsMiddleware := corsHandler.Handler(http.DefaultServeMux)
 	// Starting HTTP-server
 	log.Println("Starting server on port " + constants.DEFAULT_PORT + " ...")
-	log.Fatal(http.ListenAndServe(":"+constants.DEFAULT_PORT, nil))
+	log.Fatal(http.ListenAndServe(":"+constants.DEFAULT_PORT, corsMiddleware))
 }
